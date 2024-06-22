47d2cfd1dd83beca79a5c2c0fb84242a021c19dd
behzod_709 <behzodsharifjonov7@gmail.com>
2024-06-22 22:55:03 +0500

    first commit

--- src/containers/SecondABout/Second_About.jsx
+++ src/containers/SecondABout/Second_About.jsx
@@ -3,7 +3,19 @@ import './Second_About.css'
 
 function Second_About() {
   return (
-    <div>Second_About</div>
+    <div className='second-about'>
+      <div className="parent">
+        <div className="child">
+          <span>наше предложение</span>
+          <h3>Лучшие программы для тебя</h3>
+          <p>Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа.</p>
+          <div className="lines">
+            
+          </div>
+        </div>
+        <div className="child"></div>
+      </div>
+    </div>
   )
 }
 
